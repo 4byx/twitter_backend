4ed36462a0f34d621dc2d0a6b542afd58ef2dcd7
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-06-08 22:33:35 +0530

    basic like and user model

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,41 @@
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+      const result = await this.model.create(data);
+      return result;
+    } catch (error) {
+      console.log("something wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async destroy(id) {
+    try {
+      const result = await this.model.findByIdAndDelete({
+        id,
+      });
+      return result;
+    } catch (error) {
+      console.log("something wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const result = await this.model.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+      return result;
+    } catch (error) {
+      console.log("something wrong in repository layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = CrudRepository;
